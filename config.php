89610640389f9fd0087526ea7f078f09dc508f28
Moncho Varela <monchovarela@protonmail.com>
2020-09-16 18:11:13 +0200

    Cambiando base url a localhost:8000

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php  defined('BARRIO') or die('Sin accesso a este script.');
 
     return array(
-        "base_url" => "http://localhost/cmsbarrio",
+        "base_url" => "http://localhost:8000",
         // Theme color
         'theme_color' => '#fff',
         // background color
